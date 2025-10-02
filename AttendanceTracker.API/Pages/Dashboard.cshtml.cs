fdac33e7f76308cbc5b7709714e5de7da8498446
Jean Lucas <lo856415@gmail.com>
2025-10-02 16:24:10 -0300

    creit:pages login

--- AttendanceTracker.API/Pages/Dashboard.cshtml.cs
+++ AttendanceTracker.API/Pages/Dashboard.cshtml.cs
@@ -1,24 +1,27 @@
 using AttendanceTracker.Models.Services; // Crie esse serviço!
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using AttendanceTracker.Models.Services;
+using System.Linq;
 
-public class DashboardModel : PageModel
+namespace AttendanceTracker.API.Pages
 {
-    private readonly IAlunoService _alunoService;
+    public class DashboardModel : PageModel
+    {
+        private readonly IAlunoService _alunoService;
 
-    public DashboardModel(IAlunoService alunoService)
-    {
-        _alunoService = alunoService;
-    }
-    
-    public void OnGet()
-    {
-        var alunos = _alunoService.ListarAlunos();
-        ViewData["TotalAlunos"] = alunos.Count();
-        // Adapte os valores abaixo conforme sua lógica
-        ViewData["ValorPresentes"] = alunos.Count(a => a.Status == "Presente");
-        ViewData["ValorDispensados"] = alunos.Count(a => a.Status == "Dispensado");
-        ViewData["ValorFaltas"] = alunos.Count(a => a.Status == "Falta");
-        ViewData["ValorAlunoExtra"] = alunos.Count(a => a.Tipo == "Extra");
+        public DashboardModel(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+        
+        public void OnGet()
+        {
+            var alunos = _alunoService.ListarAlunos();
+            ViewData["TotalAlunos"] = alunos.Count();
+            // Adapte os valores abaixo conforme sua lógica
+            ViewData["ValorPresentes"] = alunos.Count(a => a.Status == "Presente");
+            ViewData["ValorDispensados"] = alunos.Count(a => a.Status == "Dispensado");
+            ViewData["ValorFaltas"] = alunos.Count(a => a.Status == "Falta");
+            ViewData["ValorAlunoExtra"] = alunos.Count(a => a.Tipo == "Extra");
+        }
     }
 }
